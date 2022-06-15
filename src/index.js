2fd6eb9e6aecbae05a46b532ab87a0eb9a785874
SteveJeong <jst0930@gmail.com>
2022-06-15 21:51:09 +0300

    #3 Setting up Firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello Firebase 9 from index.js : Hello Steve');
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0K3wfnVY_0v2PKQpaNaDWHONseB94czM",
+  authDomain: "net-ninja-firebase-9-tut.firebaseapp.com",
+  projectId: "net-ninja-firebase-9-tut",
+  storageBucket: "net-ninja-firebase-9-tut.appspot.com",
+  messagingSenderId: "528779117053",
+  appId: "1:528779117053:web:e5f41a14cc4acd1f069994"
+};
+
+initializeApp(firebaseConfig)
